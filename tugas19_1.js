3ca50b2a7f4d569214adc4d0df1e1dc9e0af4a04
Yehezkiel Reynaldi Abraham <yehezkielreynaldi01@gmail.com>
2023-04-29 20:39:09 +0700

    Tugas 19 JS
    Tugas ini saya kirimkan untuk Niomic

---
+++ tugas19_1.js
@@ -0,0 +1,8 @@
+function ulangKata(){
+
+    const kalimat1 = "Semangat..."
+    console.log(kalimat1.repeat(5));
+
+}
+
+ulangKata();
---
+++ tugas19_2.js
@@ -0,0 +1,15 @@
+function ulangBintang(){
+
+    console.log("*".repeat(5));
+    console.log("*".repeat(25));
+    console.log("*".repeat(45));
+    console.log("*".repeat(65));
+    console.log("*".repeat(75));
+    console.log("*".repeat(65));
+    console.log("*".repeat(45));
+    console.log("*".repeat(25));
+    console.log("*".repeat(5));
+
+}
+ulangBintang();
+
